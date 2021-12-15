d2f0e6ea29b98de8d2b81fab30b40db327704e95
oscargregorius <oscar.gregorius@gmail.com>
2021-12-15 15:08:46 +0100

    sqlite setup

--- foodieBackend/app.js
+++ foodieBackend/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+const db_name = path.join("./foodie.db");
+const db = new sqlite3.Database(db_name, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful connection to the database 'foodie.db'");
+});
 
 app.get("/", (req, res) => {
   res.json({ status: "connected" });
