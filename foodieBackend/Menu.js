cde1da08d2782e7772bfcc4f76c49924bb7be9d4
Ali94160 <ali.al-mousawi@hotmail.com>
2021-12-16 10:34:22 +0100

    added post and acl stuff

--- foodieBackend/Menu.js
+++ foodieBackend/Menu.js
@@ -1,12 +1,36 @@
-
-
 module.exports = (app, db) => {
-  
-  app.get("/menu", async (req, res) => {
+  app.get("/rest/menu", async (req, res) => {
     const query = "SELECT * FROM Menu";
     const list = await db.all(query);
-    res.json(data);
-  })
-  
+    res.json(list);
+  });
 
-}
+  app.post("/rest/addToMenu", async (req, res) => {
+    const menuItem = req.body;
+    const query = "INSERT INTO Menu VALUES(?,?,?)";
+
+    try {
+      const item = await db.all(query, [null, menuItem.title, menuItem.price]);
+      res.json({ status: "sucess" });
+      return;
+    } catch (err) {
+      res.json({ status: "error" });
+    }
+  });
+
+
+  app.post("/rest/addOrder", async (req, res) => {
+    const orderId = req.body;
+    const query = "SELECT * FROM Menu WHERE id = ?"
+
+    try {
+      const order = await db.all(query, [orderId])
+      
+    } catch (error) {
+      res.json({status: "Error"})
+    }
+    
+  })
+
+
+};
--- foodieBackend/acl.js
+++ foodieBackend/acl.js
@@ -3,9 +3,10 @@ const acl = require("./accessList.json");
 module.exports = (req, res, next) => {
   const path = req.path;
   const user = req.session.user;
+  const usertest = user?.role ? user.role : "no role";
 
   for (p of acl) {
-    if (p.path === path && !p.roles.includes(user.role)) {
+    if (p.path === path && !p.roles.includes(usertest)) {
       res.json({ status: "Access denied!!" });
       return;
     }
