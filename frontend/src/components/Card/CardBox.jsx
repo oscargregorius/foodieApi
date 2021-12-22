70653014767d688e4557622a296ec9d0f2a6c13f
oscargregorius <oscar.gregorius@gmail.com>
2021-12-22 11:00:01 +0100

    add and decrese btn

--- frontend/src/components/Card/CardBox.jsx
+++ frontend/src/components/Card/CardBox.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { StyledCard, StyledCardContent } from "./StyledCardBox";
+import React, { useState } from "react";
+import {
+  StyledCard,
+  StyledCardContent,
+  StyledCat,
+  StyledText,
+  StyledAmount,
+  StyledButtons,
+} from "./StyledCardBox";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { useHistory } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const CardBox = ({ item }) => {
   const history = useHistory();
+  const [amount, setAmount] = useState(0);
+
+  const handleDecrese = () => {
+    if (amount === 0) {
+      return;
+    }
+    setAmount((prev) => (prev -= 1));
+  };
 
   return (
     <StyledCard sx={{ maxWidth: 345 }}>
@@ -14,11 +30,27 @@ const CardBox = ({ item }) => {
         <Typography gutterBottom variant="h5" component="div">
           {item.title}
         </Typography>
+        <StyledCat>
+          Category: {<StyledText>{item.category}</StyledText>}
+        </StyledCat>
         <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography>
       </StyledCardContent>
+      <StyledButtons>
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={() => setAmount((prev) => (prev += 1))}
+        >
+          +
+        </Button>
+        <StyledAmount>{amount}</StyledAmount>
+        <Button variant="outlined" size="small" onClick={handleDecrese}>
+          -
+        </Button>
+      </StyledButtons>
     </StyledCard>
   );
 };
--- frontend/src/components/Card/StyledCardBox.js
+++ frontend/src/components/Card/StyledCardBox.js
@@ -4,10 +4,30 @@ import CardContent from "@mui/material/CardContent";
 
 export const StyledCard = styled(Card)`
   cursor: pointer;
+  margin-bottom: 2rem;
 `;
 
 export const StyledCardContent = styled(CardContent)`
   &:hover {
     background: orange;
   }
+`;
+
+export const StyledCat = styled.p`
+  font-size: 1rem;
+`;
+
+export const StyledText = styled.span`
+  font-size: 1rem;
+`;
+
+export const StyledButtons = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  padding: 1rem;
+`;
+
+export const StyledAmount = styled.div`
+  padding: 0.5rem;
+  justify-self: center;
 `;
